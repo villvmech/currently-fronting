041dd020012254d2614f440bfe227ca4bb2d86d4
pulchra mentis <1747037+pulchra-mentis@users.noreply.github.com>
2022-06-06 09:44:56 -0400

    Remove unused import

--- pages/[system].tsx
+++ pages/[system].tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import { useRouter } from 'next/router'
 import useSWR, { Key } from 'swr'
 import MemberCard from '../components/member-card'
