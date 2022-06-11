dca1a93b920a3ee2551b658f832174b6d4c0ae74
pulchra mentis <1747037+pulchra-mentis@users.noreply.github.com>
2022-06-11 09:40:09 +0000

    getFrontersAndSystem
    Broken code, but needed for logging changes before renaming [system].tsx

--- pages/[system].tsx
+++ pages/[system].tsx
@@ -3,48 +3,60 @@ import Head from 'next/head'
 import { useRouter } from 'next/router'
 import useSWR, { Key } from 'swr'
 import MemberCard from '../components/member-card'
-import { Switch } from '../util/types'
+import { Switch, System } from '../util/types'
 
-const getFronters = async (system: Key) => {
-  const data = await fetch(
-    `https://api.pluralkit.me/v2/systems/${system}/fronters`,
-  )
-  if (!data.ok) {
-    throw new Error(
-      "An error occurred while fetching this system's data. Perhaps their fronters are private?",
-    )
-  } else {
-    const fronters: Switch = await data.json()
-    return fronters
+interface FrontersAndSystem {
+  fronters: Switch | null
+  system: System | null
+}
+
+const getFrontersAndSystem = async (systemID: Key) => {
+  const systemData = await fetch(`https://api.pluralkit.me/v2/systems/${systemID}`)
+
+  let system: System | null = null
+  if (systemData.ok) {
+    system = await systemData.json()
   }
+  
+  const frontersData = await fetch(
+    `https://api.pluralkit.me/v2/systems/${systemID}/fronters`,
+  )
+  
+  let fronters: Switch | null = null
+  if (frontersData.ok) {
+    fronters = await frontersData.json()
+  }
+
+  return { fronters: fronters, system: system }
 }
 
 const getServerSideProps: GetServerSideProps = async context => {
-  const fronters = await getFronters(context.params?.system)
+  const { fronters, system } = await getFrontersAndSystem(context.params?.system)
 
   return {
     props: {
-      fallback: fronters,
+      fallback: { fronters, system }
     },
   }
 }
 
-const useFronters = (system: Key) => {
-  const { data } = useSWR<Switch | null>(
-    system,
-    (system: Key) => getFronters(system),
+const useFrontersAndSystem = (systemID: Key) => {
+  const { data } = useSWR<FrontersAndSystem>(
+    systemID,
+    (systemID: Key) => getFrontersAndSystem(systemID),
     { refreshInterval: 30 * 1000 },
   )
-
+  
+  const { fronters, system } = data ? data : { fronters: null, system: null }
   return {
-    fronters: data,
+    fronters, system
   }
 }
 
 const Home: NextPage = () => {
   const router = useRouter()
-  const { system } = router.query
-  const { fronters } = useFronters(system as Key)
+  const { systemID } = router.query
+  const { fronters, system } = useFrontersAndSystem(systemID as Key)
 
   return (
     <div>
@@ -64,9 +76,10 @@ const Home: NextPage = () => {
 
         {fronters &&
           fronters?.members.map(member => (
-            <MemberCard key={member.id} member={member} />
+            <MemberCard key={member.id} member={member} isSystem={false} />
           ))}
       </div>
+
       {/* prettier-ignore */}
       <footer className='container mx-auto max-w-4xl p-2 bg-slate-100 dark:bg-slate-800 text-slate-900 dark:text-slate-50 text-center rounded'>
       Powered by <span className="italic">a lot</span> of <a href="https://github.com/obscura-scripturae/currently-fronting/blob/main/package.json">open source software</a> and designed to interface with <a href="https://pluralkit.me/">PluralKit</a>. Developed by <a href="https://github.com/pulchra-mentis">pulchra mentis</a> and available under the <a href="https://github.com/obscura-scripturae/currently-fronting/blob/main/LICENSE">AGPL 3.0 license</a>. Find a bug? Follow the instructions on <a href="https://github.com/obscura-scripturae/currently-fronting">GitHub</a> in the README.
