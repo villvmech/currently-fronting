aea99cc66d65acfbb28270f097b4e2fe1217ec1d
pulchra mentis <1747037+pulchra-mentis@users.noreply.github.com>
2022-06-15 03:33:32 -0400

    Move some card contents inside banner

--- components/card-banner.tsx
+++ components/card-banner.tsx
@@ -1,19 +1,19 @@
 interface CardBannerProps {
-  name: string
   banner: string
+  children: React.ReactNode
 }
 
 const CardBanner = (props: CardBannerProps) => {
-  const { name, banner } = props
+  const { banner, children } = props
 
   return (
-    <img
-      className='max-h-48 object-cover mx-auto rounded'
-      src={banner}
-      alt={`Banner for ${name}`}
-      width={600}
-      height={240}
-    />
+    <div
+      className='h-52 min-w-full mx-auto rounded p-2 bg-no-repeat bg-center bg-cover flex flex-row place-content-center'
+      style={{ backgroundImage: `url(${banner})` }}>
+      <div className='bg-slate-300/80 dark:bg-slate-600/80 m-auto max-w-fit max-h-fit p-2 rounded'>
+        {children}
+      </div>
+    </div>
   )
 }
 
--- components/card.tsx
+++ components/card.tsx
@@ -24,12 +24,8 @@ const Card = (props: CardProps) => {
     'container max-w-md p-2 bg-slate-100 dark:bg-slate-800 text-slate-900 dark:text-slate-50 text-center rounded flex flex-col gap-2'
   return (
     <div className={cardClasses}>
-      {banner && <CardBanner name={name} banner={banner} />}
-      <div>
-        {avatar_url && (
-          <CardAvatar name={name} avatar_url={avatar_url} color={color} />
-        )}
-        <div>
+      {banner && (
+        <CardBanner banner={banner}>
           <h1 className='text-xl font-bold'>{name}</h1>
           {display_name && display_name && (
             <h2 className='text-lg italic'>{display_name}</h2>
@@ -41,6 +37,28 @@ const Card = (props: CardProps) => {
             />
           )}
           {birthday && <div className='italic'>{birthday}</div>}
+        </CardBanner>
+      )}
+      <div>
+        {avatar_url && (
+          <CardAvatar name={name} avatar_url={avatar_url} color={color} />
+        )}
+        <div>
+          {!banner && (
+            <div>
+              <h1 className='text-xl font-bold'>{name}</h1>
+              {display_name && display_name && (
+                <h2 className='text-lg italic'>{display_name}</h2>
+              )}
+              {pronouns && (
+                <CardPronouns
+                  pronouns={pronouns}
+                  htmlReactParserOptions={DiscordMarkdownParserOptions}
+                />
+              )}
+              {birthday && <div className='italic'>{birthday}</div>}
+            </div>
+          )}
           {description && (
             <CardDescription
               description={description}
