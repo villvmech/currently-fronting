e07ea4845b37fac80f46ae27709f20b27e09002e
pulchra mentis <1747037+pulchra-mentis@users.noreply.github.com>
2022-06-17 04:33:34 -0400

    Use Discord media cache when/where possible

--- components/card-banner.tsx
+++ components/card-banner.tsx
@@ -1,3 +1,5 @@
+import DiscordCDNToMediaCache from '../util/discord-cdn-to-media-cache'
+
 interface CardBannerProps {
   banner: string
   children?: React.ReactNode
@@ -5,11 +7,15 @@ interface CardBannerProps {
 
 const CardBanner = (props: CardBannerProps) => {
   const { banner, children } = props
+  const resizedBannerURL = `${banner.replace(
+    DiscordCDNToMediaCache,
+    'https://media.discordapp.net/',
+  )}${children ? '?width=1200&height=480' : '?width=600&height=240'}`
 
   return children ? (
     <div
       className='h-52 min-w-full mx-auto rounded p-2 bg-no-repeat bg-center bg-cover flex flex-row place-content-center'
-      style={{ backgroundImage: `url(${banner})` }}>
+      style={{ backgroundImage: `url(${resizedBannerURL})` }}>
       <div className='bg-slate-300/80 dark:bg-slate-600/80 m-auto max-w-fit max-h-fit p-2 rounded'>
         {children}
       </div>
@@ -17,7 +23,7 @@ const CardBanner = (props: CardBannerProps) => {
   ) : (
     <img
       className='max-h-48 object-cover mx-auto rounded'
-      src={banner}
+      src={resizedBannerURL}
       alt={`Banner for ${name}`}
       width={600}
       height={240}
